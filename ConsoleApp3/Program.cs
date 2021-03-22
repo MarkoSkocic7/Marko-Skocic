5bdae2fc9b723067f0fa190fa5364ad936dcf5ba
Maras3012 <60932373+Maras3012@users.noreply.github.com>
2021-03-22 20:20:05 +0100

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Novo");
+            Console.WriteLine("Novo"); //glup si
         }
     }
 }
